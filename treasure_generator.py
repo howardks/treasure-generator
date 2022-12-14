df95ced5e1487803eb60f6c2274067f3f089bed5
howardks <93079698+howardks@users.noreply.github.com>
2022-12-14 09:08:33 -0500

    Add files via upload

---
+++ treasure_generator.py
@@ -0,0 +1,22 @@
+import random
+
+name_list = list()
+adj_list = list()
+
+with open("nameList.txt") as names :
+    for x in names :
+        name_list.append(names.readline().strip()) 
+
+with open("adjectiveList.txt") as adjs :
+    for x in adjs:
+        adj_list.append(adjs.readline().strip())
+
+print(name_list)
+print(adj_list)
+
+user_num = input("How many items: ")
+print("\nTreasure: ")
+
+for x in range(int(user_num)) :
+    value = str(random.randint(0, 1)) + "gp " + str(random.randint(0, 5)) + "sp " + str(random.randint(0, 9)) + "cp"
+    print(random.choice(adj_list) + " " + random.choice(name_list) + "\t\tValue: " + value)
